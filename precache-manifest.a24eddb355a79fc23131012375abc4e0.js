fbc6adad8199e7d2b6aeaf05115ceb17d6248e01
Mayank Singh Fartiyal <mnksingh1996@gmail.com>
2020-08-29 03:21:08 +0530

    Updates

---
+++ precache-manifest.a24eddb355a79fc23131012375abc4e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30634cdff8482afc03c340dde189461e",
+    "url": "/excel-reader-react/index.html"
+  },
+  {
+    "revision": "4b3dc241b02ed5780716",
+    "url": "/excel-reader-react/static/css/main.6dccf4c5.chunk.css"
+  },
+  {
+    "revision": "f2e9516168496f44b5ec",
+    "url": "/excel-reader-react/static/js/2.ebe70e8a.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/excel-reader-react/static/js/2.ebe70e8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b3dc241b02ed5780716",
+    "url": "/excel-reader-react/static/js/main.b70a66ef.chunk.js"
+  },
+  {
+    "revision": "860c190e161d470c8967",
+    "url": "/excel-reader-react/static/js/runtime-main.00da0577.js"
+  }
+]);
